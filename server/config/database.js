32a322685d8b46742d55b644237d6a7948bbdd7c
deepesh11yadav <165560646+deepesh11yadav@users.noreply.github.com>
2024-07-13 15:45:47 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -9,8 +9,10 @@ exports.connect = () => {
       useNewUrlParser: true, // Corrected typo here
       useUnifiedTopology: true,
     })
-    .then(() => {
+    .then((f) => {
       console.log(`DB Connection Success`); // Moved the console.log inside the then block
+      console.log(MONGODB_URL);
+      console.log(f);
     })
     .catch((err) => {
       console.log(`DB Connection Failed`);
